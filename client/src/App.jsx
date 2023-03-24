161210dc772d1e680a9c42c07dc7a9337cf0d642
darkolol9 <whinedine8@gmail.com>
2023-03-24 12:33:29 +0300

    added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,21 @@ import ChangePassword from "./Pages/ChangePassword/ChangePassword";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import Register from "./Pages/Register/Register";
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
 function App() {
   return (
-    <div className="App">
-      {/* <Register />
-       */}
-      {/* <ChangePassword /> */}
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot_password" element={<ChangePassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<h1>Nothing</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
