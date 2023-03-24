49cc188070d8cc89049244613c1d49e955ccfbfe
darkolol9 <whinedine8@gmail.com>
2023-03-24 14:25:00 +0300

    more css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,8 +3,10 @@ import NavbarItem from "./NavbarItem";
 const NavBar = () => {
   return (
     <div className="navbar">
+      <NavbarItem label="Home" url="/*" />
       <NavbarItem label="Login" url="/login" />
       <NavbarItem label="Register" url="/register" />
+      <NavbarItem label="Forgot Password" url="/forgot_password" />
     </div>
   );
 };
