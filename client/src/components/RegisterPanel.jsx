cfff8fead083f1f1a7402cda23344dcc6ebd4779
darkolol9 <whinedine8@gmail.com>
2023-03-24 12:11:33 +0300

    s

--- client/src/components/RegisterPanel.jsx
+++ client/src/components/RegisterPanel.jsx
@@ -1,20 +1,27 @@
 import InputField from "./InputField";
 import SubmitButton from "./SubmitButton";
 
+const IMG_URL = "https://media.giphy.com/media/DHeDr3SAM08koK2GFT/giphy.gif";
+
 const RegisterPanel = () => {
   return (
     <div>
       <div className="register-panel">
         <div className="register-form">
           <h1 className="register-title">Register</h1>
-          <InputField fieldName="Email" />
-          <InputField fieldName="Username" />
-          <InputField fieldName="Password" />
-          <InputField fieldName="Repeat password" />
 
-          <SubmitButton />
+          <div className="form">
+            <InputField fieldName="Email" />
+            <InputField fieldName="Username" />
+            <InputField fieldName="Password" />
+            <InputField fieldName="Repeat password" />
+
+            <SubmitButton />
+          </div>
         </div>
-        <div className="register-pic">PICTURE</div>
+        <div className="register-pic">
+          <img className="register-pic" src={IMG_URL} alt="" />
+        </div>
       </div>
     </div>
   );
