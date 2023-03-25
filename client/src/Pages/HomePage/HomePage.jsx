fe44d1f67fb64050891b9a25167a87f0b3672c88
darkolol9 <whinedine8@gmail.com>
2023-03-25 16:16:25 +0300

    css

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <>
       <div className="container-wrap col">
-        <h1 className="text-body-secondary">Welcome to Communications Ltd</h1>
+        <h1 className="text-body-secondary title">Welcome to Communications Ltd</h1>
         <div>
-          <h2>About Us</h2>
+          <h2 className="title">About Us</h2>
           <p>
             Communications Ltd is a leading provider of communication services
             for businesses of all sizes. Our goal is to help our clients improve
             their communication capabilities and increase their productivity.
           </p>
-          <h2>Services</h2>
+          <h2 className="title">Services</h2>
           <ul>
             <li>VoIP Phone Systems</li>
             <li>Unified Communications</li>
             <li>Video Conferencing</li>
             <li>Contact Center Solutions</li>
           </ul>
-          <h2>Contact Us</h2>
+          <h2 className="title">Contact Us</h2>
           <p>Want to learn more about our services? Contact us today!</p>
           <div>
             Communications Ltd 123 Main St Anytown, USA Phone: 555-555-5555
