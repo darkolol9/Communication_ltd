b03ca910427d2575ade08d13ea49c3cf34838b18
darkolol9 <whinedine8@gmail.com>
2023-05-12 10:10:36 +0300

    added config

--- client/src/components/RegisterPanel.jsx
+++ client/src/components/RegisterPanel.jsx
@@ -1,27 +1,48 @@
 import InputField from "./InputField";
 import SubmitButton from "./SubmitButton";
+import * as Config from "../config.json";
+import { useState } from "react";
+import { validateByConfig } from "../helpers";
 
 const IMG_URL = "https://media.giphy.com/media/DHeDr3SAM08koK2GFT/giphy.gif";
 
+
+const onRegister = async (formData) => {
+
+  let isPasswordValid = validateByConfig(formData);
+
+  if (!isPasswordValid) {
+    console.log("YOU SUCK, THE DETAILS ARE NOT GOOD");
+  }
+};
+
 const RegisterPanel = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+
   return (
-      <div className="register-panel">
-        <div className="register-form">
-          <h1 className="register-title">Register</h1>
+    <div className="register-panel">
+      <div className="register-form">
+        <h1 className="register-title">Register</h1>
 
-          <div className="form">
-            <InputField fieldName="Email" />
-            <InputField fieldName="Username" />
-            <InputField fieldName="Password" />
-            <InputField fieldName="Repeat password" />
+        <div className="form">
+          <InputField fieldName="Email" onChange={(e) => setFormData({...formData, email : e.target.value})} />
+          <InputField fieldName="Username" onChange={(e) => setFormData({...formData, username : e.target.value})}/>
+          <InputField fieldName="Password" onChange={(e) => setFormData({...formData, password : e.target.value})}/>
+          <InputField fieldName="Repeat password" onChange={(e) => setFormData({...formData, repeatPassword : e.target.value})} />
 
-            <SubmitButton />
-          </div>
-        </div>
-        <div>
-          <img className="register-pic" src={IMG_URL} alt="" />
+          <SubmitButton onClick={() => onRegister(formData)} />
         </div>
       </div>
+      <div>
+        <img className="register-pic" src={IMG_URL} alt="" />
+      </div>
+    </div>
   );
 };
 
---
+++ client/src/helpers.js
@@ -0,0 +1,49 @@
+import * as Config from "../src/config.json";
+
+const passwordConfig = Config.default;
+
+const containsCaptial = (data) => {
+    return /[A-Z]/.test(data);
+}
+
+const containsDigits = (data) => {
+    return /[0-9]/.test(data);
+}
+
+const containsSpecialChars = (data) => {
+    return /[\W_]/.test(data);
+}
+
+
+export const validateByConfig = (formData) => {
+
+    if (passwordConfig.passwordLength > formData.password) {
+        console.log("len")
+        return false;
+    }
+
+    if (passwordConfig.mustContain.capitals) {
+        if (!containsCaptial(formData.password)) {
+            return false;
+        }
+    }
+
+    if (passwordConfig.mustContain.digits) {
+        if (!containsDigits(formData.password)) {
+            return false;
+        }
+    }
+
+    if (passwordConfig.mustContain.specialChars) {
+        if (!containsSpecialChars(formData.password)) {
+            return false;
+        }
+    }
+
+    if (formData.password !== formData.repeatPassword) {
+        return false;
+    }
+
+    return true;
+
+}
