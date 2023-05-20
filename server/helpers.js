998c8ffc2898b37b976293e164d647a86521f33b
darkolol9 <whinedine8@gmail.com>
2023-05-20 13:42:48 +0300

    S

--- server/helpers.js
+++ server/helpers.js
@@ -1,4 +1,19 @@
 const crypto = require("crypto");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(
+  "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"
+);
+
+async function sendEmail(msg) {
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent successfully");
+    })
+    .catch((error) => {
+      console.log("Error sending email:", error);
+    });
+}
 
 function hashPassword(password) {
   // Generate a random salt
@@ -30,4 +45,5 @@ function checkPassword(password, salt, hashed_password) {
 module.exports = {
   hashPassword,
   checkPassword,
+  sendEmail,
 };
